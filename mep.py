f04b5c2570b3a2402c87b0fa582aad6bc6118e1c
Vit Cerny <vit.cerny1@protonmail.com>
2021-06-17 19:41:42 +0200

    big redesign, working well

--- mep.py
+++ mep.py
@@ -1,12 +1,7 @@
 # TO DO
-## - redo TIME SPAN to be more customizable
-##    - that will mean deleting ct and getting and implemanting date_time at all circumstances as the ultimate variable for setting the desired date
-##    - the goal is that a user can customize exactly his desired date
-##    - might not eventually do it as a person that knows shell scripting doesn't really need these
-##    - another option is implementing this extra functionality in a shell script, which would be easier as far as I'm concerned.
-## - month option
-## - year option
-##
+## - test heavily
+## - write a README and a test file
+## - create an export option, something that would export the events to JSON or iCal eventually
 #
 # Name: mep - markdown events with python
 # Author: Vít Černý
@@ -16,6 +11,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import datetime
+from dateutil.relativedelta import relativedelta
 
 event = {
     0: '',  # date
@@ -32,33 +28,36 @@ event_time = {
 }
 
 date_time = {
-    0: 0,  # year
-    1: 0,  # month
-    2: 0  # day
+    0: datetime.datetime.today().year,  # year
+    1: datetime.datetime.today().month,  # month
+    2: datetime.datetime.today().day  # day
 }
 
-week_number =  int(datetime.datetime.now().strftime("%V"))
+date_date = datetime.datetime.today()
+week_number =  int(date_date.strftime("%V"))
+
+day_used = False
+last_day = ' '
 
 def print_help():
-    print('Usage: mep [FILE] [TIME SPAN]')
+    print('Usage: mep [FILE] [TIMESPAN]')
     print('\tEnter \'mep help\' to print this menu')
     print('\t[FILE] - path to a file')
-    print('\t[TIME SPAN] - options: ')
+    print('\t[TIMESPAN] - options: ')
     print('\t\tall - all events')
-    print('\t\ttoday - all events for today')
-    print('\t\ttomorrow - all events for tomorrow')
-    print('\t\tweek [+-NUMBER] (optional, works only within current year) - all events for a certain week')
-    print('\t\tmonth [+-NUMBER] (optional, works only within current year) - all events for a certain month')
-    print('\t\tdate [YYYY-MM-DD] - all events for a day')
+    print('\t\tday [+-NUMBER] (optional) - all events for a day')
+    print('\t\tweek [+-NUMBER] (optional) - all events for a week')
+    print('\t\tmonth [+-NUMBER] (optional) - all events for a month')
+    print('\t\tyear [+-NUMBER] (optional) - all events for a year')
+    print('\t\tdate [YYYY-MM-DD] - all events for a day, input by date')
 
 
 
-def time_to_dic(event: dict):
+def time_to_dic(full_date, output: dict):
     # first clean the dictionary
     for attr in event_time:
-        event_time[attr] = 0
+        output[attr] = 0
     # get values
-    full_date = event[0]
     indexer = 0
     for ch in full_date:
         # print(ch)
@@ -67,103 +66,137 @@ def time_to_dic(event: dict):
             if indexer > 2:
                 break
             continue
-        event_time[indexer] *= 10
-        event_time[indexer] += int(ch)
-
-
-def date_to_dic(certain_date):
-    indexer = 0
-    for ch in certain_date:
-        # print(ch)
-        if ch == '-':
-            indexer += 1
-            if indexer > 2:
+        if indexer > 1:
+            try:
+                int(ch)
+            except ValueError:
                 break
-            continue
-        date_time[indexer] *= 10
-        date_time[indexer] += int(ch)
-
-
+        output[indexer] *= 10
+        output[indexer] += int(ch)
 
 def decide_print(timespan):
     print_permit = False
-    if timespan == "date":
+    if timespan == "all":
+        print_permit == True
+    if timespan == "date" or "day":
         if date_time[0] == event_time[0] and date_time[1] == event_time[1] and date_time[2] == event_time[2]:
             print_permit = True
-            return print_permit
-    ct = datetime.datetime.now()  # ct stands for current time
-    if timespan == "all":
-        print_permit = True
-    if timespan == "today":
-        if ct.year == event_time[0] and ct.month == event_time[1] and ct.day == event_time[2]:
-            print_permit = True
-    if timespan == "tomorrow":
-        if ct.year == event_time[0] and ct.month == event_time[1] and ct.day+1 == event_time[2]:
-            print_permit = True
     if timespan == "week":
-        if week_number == int(datetime.date(event_time[0], event_time[1], event_time[2]).strftime("%V")):
+        if date_time[0] == event_time[0] and week_number == int(datetime.date(event_time[0], event_time[1], event_time[2]).strftime("%V")):
+            print_permit = True
+    if timespan == "month":
+        if date_time[0] == event_time[0] and date_time[1] == event_time[1]:
+            print_permit = True
+    if timespan == "year":
+        if date_time[0] == event_time[0]:
             print_permit = True
     return print_permit
 
 
 def print_output(event):
+    global day_used, last_day
     day = datetime.datetime(event_time[0], event_time[1], event_time[2])
-    print('On', day.strftime('%A') + ',', (str(event_time[1]) + '/' + str(event_time[2]) + '/' + str(event_time[0])) + ',', 'at', event[1] + ':')
-    print('\t'+ event[2], '(' + event[3] + ')')
-    print('\t'+ event[4], '\n')
+    if day != last_day:
+        day_used = False
+    if day_used == False:
+        print('\nOn', day.strftime('%A') + ',', (str(event_time[1]) + '/' + str(event_time[2]) + '/' + str(event_time[0])) + ':')
+        day_used = True
+    print('event:', event[2], '(' + event[3] + ')', 'at', event[1])
+    print('\t'+ event[4])
+    last_day = day
+
+
+def parse_shift(arg, output):
+    global date_date
+    global week_number
+    # parsing shift
+    to_add = 0
+    for char in arg:
+        to_add *= 10
+        try:
+            if arg[0] == '-':
+                to_add -= int(char)
+            else:
+                to_add += int(char)
+        except ValueError:
+            continue
+    # outputing shift
+    if output == 2:
+        date_date += relativedelta(days=to_add)
+    if output == 1:
+        date_date += relativedelta(months=to_add)
+    if output == 0:
+        print('hey')
+        date_date += relativedelta(years=to_add)
+    if output == "week_number":
+        date_date += relativedelta(weeks=to_add)
+        week_number = int(date_date.strftime("%V"))
+    time_to_dic(str(date_date), date_time)
+
+
+def parse_timespan():
+    timespan = sys.argv[2]
+    argv_len = len(sys.argv)
+
+    if timespan == "date":
+        try:
+            time_to_dic(sys.argv[3], date_time)
+        except IndexError:
+            print("Argument missing for date!")
+            sys.exit()
+        return timespan
+    if timespan == "day":
+        if argv_len > 3:
+            parse_shift(sys.argv[3], 2)
+        return timespan
+    if timespan == "week":
+        if argv_len > 3:
+            parse_shift(sys.argv[3], "week_number")
+        return timespan
+    if timespan == "month":
+        if argv_len > 3:
+            parse_shift(sys.argv[3], 1)
+        return timespan
+    if timespan == "year":
+        if argv_len > 3:
+            parse_shift(sys.argv[3], 0)
+        return timespan
+    if timespan == "all":
+        return timespan
+    print('Invalid argument!')
+    sys.exit()
+
 
 def main():
+
     # Read command-line args
     ## help
     if len(sys.argv) < 2:
         print_help()
-        exit()
+        sys.exit()
     if sys.argv[1] == "help":
         print_help()
-        exit()
+        sys.exit()
     ## filename
     try:
         filename = sys.argv[1]
     except IndexError:
         print('Enter a file name!')
-        exit()
-    ## timespan
+        sys.exit()
+    ## set timespan
     if len(sys.argv) > 2:
-        timespan = sys.argv[2]
+        timespan = parse_timespan()
     else:
         ch = input('Proceed to display all events? (y/n) ')
         timespan = "all"
         if ch != 'y':
             exit()
-    ### date
-    if timespan == "date":
-        try:
-            certain_date = sys.argv[3]
-        except IndexError:
-            print("Argument missing for date!")
-            exit()
-        date_to_dic(certain_date)
-    ### week
-    if timespan == "week" and len(sys.argv) > 3:
-        global week_number
-        add_to_week = 0
-        for char in sys.argv[3]:
-            add_to_week *= 10
-            try:
-                if sys.argv[3][0] == '-':
-                    add_to_week -= int(char)
-                else:
-                    add_to_week += int(char)
-            except ValueError:
-                continue
-        week_number += add_to_week
     # Open the file
     try:
         fp = open(filename, 'r')
     except FileNotFoundError:
         print('File does not exist!')
-        exit()
-
+        sys.exit()
     # Read and print output
     while True:
         ch = fp.read(1)
@@ -182,7 +215,7 @@ def main():
                 tch = fp.read(1)
                 if cuch == ';' and tch == ';':
                     event[event_attr] = data.strip()
-                    time_to_dic(event)
+                    time_to_dic(event[0], event_time)
                     print_permit = decide_print(timespan)
                     if print_permit:
                         print_output(event)
