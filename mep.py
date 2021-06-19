af4b40937bdeb9df635c5251e7ebad821b44863e
Vit Cerny <vit.cerny1@protonmail.com>
2021-06-19 14:27:29 +0200

    minor bug fixed and better code documentation

--- mep.py
+++ mep.py
@@ -14,46 +14,45 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 event = {
-    0: '',  # date
-    1: '',  # time
-    2: '',  # name
-    3: '',  # place
-    4: ''  # additional info
+    0: "",  # date
+    1: "",  # time
+    2: "",  # name
+    3: "",  # place
+    4: "",  # additional info
 }
 
-event_time = {
-    0: 0,  # year
-    1: 0,  # month
-    2: 0  # day
-}
+event_time = {0: 0, 1: 0, 2: 0}  # year  # month  # day
 
 date_time = {
     0: datetime.datetime.today().year,  # year
     1: datetime.datetime.today().month,  # month
-    2: datetime.datetime.today().day  # day
+    2: datetime.datetime.today().day,  # day
 }
 
 date_date = datetime.datetime.today()
-week_number =  int(date_date.strftime("%V"))
+week_number = int(date_date.strftime("%V"))
 
+# pylint: disable=invalid-name
 day_used = False
-last_day = ' '
+last_day = " "
+
 
 def print_help():
-    print('Usage: mep [FILE] [TIMESPAN]')
-    print('\tEnter \'mep help\' to print this menu')
-    print('\t[FILE] - path to a file')
-    print('\t[TIMESPAN] - options: ')
-    print('\t\tall - all events')
-    print('\t\tday [+-NUMBER] (optional) - all events for a day')
-    print('\t\tweek [+-NUMBER] (optional) - all events for a week')
-    print('\t\tmonth [+-NUMBER] (optional) - all events for a month')
-    print('\t\tyear [+-NUMBER] (optional) - all events for a year')
-    print('\t\tdate [YYYY-MM-DD] - all events for a day, input by date')
-
+    """prints help"""
+    print("Usage: mep [FILE] [TIMESPAN]")
+    print("\tEnter 'mep help' to print this menu")
+    print("\t[FILE] - path to a file")
+    print("\t[TIMESPAN] - options: ")
+    print("\t\tall - all events")
+    print("\t\tday [+-NUMBER] (optional) - all events for a day")
+    print("\t\tweek [+-NUMBER] (optional) - all events for a week")
+    print("\t\tmonth [+-NUMBER] (optional) - all events for a month")
+    print("\t\tyear [+-NUMBER] (optional) - all events for a year")
+    print("\t\tdate [YYYY-MM-DD] - all events for a day, input by date")
 
 
 def time_to_dic(full_date, output: dict):
+    """outputs date to a dict of a certain format"""
     # first clean the dictionary
     for attr in event_time:
         output[attr] = 0
@@ -61,7 +60,7 @@ def time_to_dic(full_date, output: dict):
     indexer = 0
     for ch in full_date:
         # print(ch)
-        if ch == '-':
+        if ch == "-":
             indexer += 1
             if indexer > 2:
                 break
@@ -74,15 +73,23 @@ def time_to_dic(full_date, output: dict):
         output[indexer] *= 10
         output[indexer] += int(ch)
 
+
 def decide_print(timespan):
+    """decides whether a currently parsed event should be printed according to timespan"""
     print_permit = False
     if timespan == "all":
-        print_permit == True
-    if timespan == "date" or "day":
-        if date_time[0] == event_time[0] and date_time[1] == event_time[1] and date_time[2] == event_time[2]:
+        print_permit = True
+    if timespan in ("date", "day"):
+        if (
+            date_time[0] == event_time[0]
+            and date_time[1] == event_time[1]
+            and date_time[2] == event_time[2]
+        ):
             print_permit = True
     if timespan == "week":
-        if date_time[0] == event_time[0] and week_number == int(datetime.date(event_time[0], event_time[1], event_time[2]).strftime("%V")):
+        if date_time[0] == event_time[0] and week_number == int(
+            datetime.date(event_time[0], event_time[1], event_time[2]).strftime("%V")
+        ):
             print_permit = True
     if timespan == "month":
         if date_time[0] == event_time[0] and date_time[1] == event_time[1]:
@@ -93,20 +100,34 @@ def decide_print(timespan):
     return print_permit
 
 
-def print_output(event):
+# I don't wanna pollute main, so I'm using global variables, it's also more tidy imo
+# pylint: disable=global-statement, invalid-name
+def print_output(certain_event):
+    """prints output as requiered by user"""
     global day_used, last_day
+
     day = datetime.datetime(event_time[0], event_time[1], event_time[2])
     if day != last_day:
         day_used = False
-    if day_used == False:
-        print('\nOn', day.strftime('%A') + ',', (str(event_time[1]) + '/' + str(event_time[2]) + '/' + str(event_time[0])) + ':')
+    if day_used is False:
+        print(
+            "\n",
+            day.strftime("%A") + ",",
+            (str(event_time[1]) + "/" + str(event_time[2]) + "/" + str(event_time[0]))
+            + ":",
+        )
         day_used = True
-    print('event:', event[2], '(' + event[3] + ')', 'at', event[1])
-    print('\t'+ event[4])
+
+    print(
+        "event:", certain_event[2], "(" + certain_event[3] + ")", "at", certain_event[1]
+    )
+    print("\t" + certain_event[4])
     last_day = day
 
 
+# pylint: disable=global-statement, invalid-name
 def parse_shift(arg, output):
+    """parses relative shift of timespan as defined in args"""
     global date_date
     global week_number
     # parsing shift
@@ -114,7 +135,7 @@ def parse_shift(arg, output):
     for char in arg:
         to_add *= 10
         try:
-            if arg[0] == '-':
+            if arg[0] == "-":
                 to_add -= int(char)
             else:
                 to_add += int(char)
@@ -126,7 +147,6 @@ def parse_shift(arg, output):
     if output == 1:
         date_date += relativedelta(months=to_add)
     if output == 0:
-        print('hey')
         date_date += relativedelta(years=to_add)
     if output == "week_number":
         date_date += relativedelta(weeks=to_add)
@@ -135,6 +155,7 @@ def parse_shift(arg, output):
 
 
 def parse_timespan():
+    """parses timespan args and outputs to dicts"""
     timespan = sys.argv[2]
     argv_len = len(sys.argv)
 
@@ -163,12 +184,13 @@ def parse_timespan():
         return timespan
     if timespan == "all":
         return timespan
-    print('Invalid argument!')
+    print("Invalid argument!")
     sys.exit()
 
 
+# pylint: disable=too-many-statements,too-many-branches, invalid-name
 def main():
-
+    """ Main function, where it all gets done, used to from C, more tidy imo """
     # Read command-line args
     ## help
     if len(sys.argv) < 2:
@@ -181,39 +203,42 @@ def main():
     try:
         filename = sys.argv[1]
     except IndexError:
-        print('Enter a file name!')
+        print("Enter a file name!")
         sys.exit()
     ## set timespan
     if len(sys.argv) > 2:
         timespan = parse_timespan()
     else:
-        ch = input('Proceed to display all events? (y/n) ')
+        ch = input("Proceed to display all events? (y/n) ")
         timespan = "all"
-        if ch != 'y':
-            exit()
+        if ch != "y":
+            sys.exit()
     # Open the file
+    # pylint: disable=consider-using-with
     try:
-        fp = open(filename, 'r')
+        fp = open(filename, "r")
     except FileNotFoundError:
-        print('File does not exist!')
+        print("File does not exist!")
         sys.exit()
     # Read and print output
     while True:
         ch = fp.read(1)
-        if ch == '':
+        if ch == "":
             break
         # read event entry and output to event dict
-        if ch == ';' and fp.read(1) == ';':
-            # Initialize variables
-            cuch = ''  # cuch stands for current char as ch is already taken in this scope
-            data = ''
+        if ch == ";" and fp.read(1) == ";":
+            # Initialize some variables now
+            data = ""
             event_attr = 0
             # read event parameters till the event end
             while True:
+                # cuch stands for current char as ch is already taken in this scop
                 cuch = fp.read(1)
                 pos = fp.tell()
                 tch = fp.read(1)
-                if cuch == ';' and tch == ';':
+                if cuch == "":
+                    break
+                if cuch == ";" and tch == ";":
                     event[event_attr] = data.strip()
                     time_to_dic(event[0], event_time)
                     print_permit = decide_print(timespan)
@@ -221,12 +246,12 @@ def main():
                         print_output(event)
                     break
                 fp.seek(pos)
-                if cuch == '/':
+                if cuch == "/":
                     if event_attr > 4:
                         break
                     event[event_attr] = data.strip()
-                    data = ''
-                    cuch = ''
+                    data = ""
+                    cuch = ""
                     event_attr += 1
                 data += cuch
 
